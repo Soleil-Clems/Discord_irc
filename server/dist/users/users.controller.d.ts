cef9c4ff463494a360a17a17e58b35c9f681124d
Soleil-Clems <ouisolclems@gmail.com>
2026-01-26 16:12:32 +0100

    init project

---
+++ server/dist/users/users.controller.d.ts
@@ -0,0 +1,8 @@
+import { UsersService } from './users.service';
+export declare class UsersController {
+    private readonly usersService;
+    constructor(usersService: UsersService);
+    findAll(): string;
+    findOne(id: string): string;
+    remove(id: string): string;
+}
---
+++ server/dist/users/users.service.d.ts
@@ -0,0 +1,5 @@
+export declare class UsersService {
+    findAll(): string;
+    findOne(id: number): string;
+    remove(id: number): string;
+}
